8a3df5943195c10803c2be0703466f3bc3e25c82
Suhair Zain <suhairzain11@gmail.com>
2021-07-24 19:21:08 +0530

    Auto formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      where: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:1234')
+      .then(response => response.json())
+      .then(data => { console.log(data); this.setState({ where: data.where }) });
+  }
+
+  render() {
+    const { where }= this.state;
+
+    if (!where) {
+      return null;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to {where}.
+          </p>
+          <p>
+            <img src={logo} className="App-logo" alt="logo" />
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
